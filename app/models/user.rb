c560ed513340ef286c66b150bfaa30327f6a856c
Timo Engelhardt <Timo.Engelhardt@gmail.com>
2016-05-16 11:42:46 +0200

    formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
     risk_reasons = castle_security_context.risk_reasons
     approve_url = castle_security_context.approve_url
     deny_url  = castle_security_context.deny_url
+    
     UserMailer.suspicious_activity(self, risk_reasons, approve_url, deny_url).deliver_later
   end
 end
