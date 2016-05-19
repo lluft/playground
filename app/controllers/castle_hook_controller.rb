ead218dc9dcd9be7d8e27fdff3ed1482918c4e71
Timo Engelhardt <Timo.Engelhardt@gmail.com>
2016-05-19 14:57:37 +0200

    added signature verification to webhook

--- app/controllers/castle_hook_controller.rb
+++ app/controllers/castle_hook_controller.rb
@@ -1,5 +1,21 @@
 class CastleHookController < ApplicationController
   skip_before_filter :verify_authenticity_token
+  before_filter :verify_webhook
+
+  def verify_webhook
+    data = request.body.read
+    hmac_header = request.headers["X-Castle-Signature"]
+    digest  = OpenSSL::Digest::Digest.new('sha256')
+
+    calculated_hmac =
+    Base64.encode64(OpenSSL::HMAC.digest(
+      digest,
+      ENV["CASTLE_API_SECRET"],
+      data)
+    ).strip
+
+    calculated_hmac == hmac_header
+  end
 
   def post
     # Retrieve the request's body and parse it as JSON
