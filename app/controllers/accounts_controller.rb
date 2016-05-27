731784459d14548126adb49ae3ad6ecab3bd2e7d
Timo Engelhardt <Timo.Engelhardt@gmail.com>
2016-05-27 13:27:18 +0200

    added static device constructor: store device on session

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,24 +3,20 @@ class AccountsController < ApplicationController
 
   def authorize
     super
-    castle_session = CastleSession.find_by_session_id(session.id)
-    
-    if castle_session == nil
-      authentication = castle.authentications.create(user_id: current_user.id)
-      castle_session = CastleSession.new(session.id, authentication.device_id, authentication.risk, current_user.id)
-      castle_session.save
-    end
 
-    redirect_to challenge_path if castle_session.status.challenged?
-    redirect_to blocked_path if castle_session.status.blocked?
+    device = Device.find_by_castle_id(session[:castle_id])
+    device ||= Device.resolve(current_user, castle)
+    session[:castle_id] = device.castle_id
+
+    # redirect_to challenge_path if device.challenged?
+    # redirect_to blocked_path if device.blocked?
   end
-  
 
   def update
     if current_user.update_attributes params.require(:user).permit(:description)
-      redirect_to account_path, notice: "Saved!"
+      redirect_to account_path, notice: 'Saved!'
     else
-      redirect_to account_path, notice: "Something went wrong."
+      redirect_to account_path, notice: 'Something went wrong.'
     end
   end
 end
--- app/models/device.rb
+++ app/models/device.rb
@@ -1,15 +1,29 @@
 class Device < ActiveRecord::Base
   belongs_to :user
+  has_many :sessions, class_name: ActiveRecord::SessionStore::Session
 
-  def self.resolve(user, session_id)
-    Device.find_or_create_by(user_id: user.id)
+  def self.resolve(user, castle)
+    authentication = castle.authentications.create(user_id: user.id)
+    device = Device.find_by castle_id: authentication.device_id
+    return device if device
+
+    device = Device.create(user_id: user.id, castle_id: authentication.device_id)
+    if authentication.risk > 0.9
+      user.lock!
+    elsif authentication.risk > 0.6
+      device.challenge!
+    elsif authentication.risk > 0.3
+      # Notify the user about unusual activity
+      user.send_notification_email(auth)
+    end
+    device
   end
 
   def challenge!
     return if challenge_token.present?
     
-    challenge_token = 123456789
-    notify_user_with_challenge(challenge_token)
+    challenge_token = 123_456_789
+    user.notify_user_with_challenge(challenge_token)
   end
 
   def challenge?
