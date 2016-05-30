d0e683cf757ab526bfe719325a3323dd8c22426a
Timo Engelhardt <Timo.Engelhardt@gmail.com>
2016-05-30 10:45:09 +0200

    redirect to account page when refreshing challenge page after resolving

--- app/controllers/active_challenges_controller.rb
+++ app/controllers/active_challenges_controller.rb
@@ -1,2 +1,10 @@
 class ActiveChallengesController < ApplicationController
+  before_filter :challenge_resolved
+
+  def challenge_resolved
+    if session[:castle_id]
+      device = Device.find_by castle_id: session[:castle_id]
+      redirect_to account_path unless device.challenge?
+    end
+  end
 end
