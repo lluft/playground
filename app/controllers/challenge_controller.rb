4c264e6bdd353ab18196b3703cee92821574e02c
Timo Engelhardt <Timo.Engelhardt@gmail.com>
2016-06-19 20:22:52 +0200

    remove unneeded user_id in challenge flow

--- app/controllers/challenge_controller.rb
+++ app/controllers/challenge_controller.rb
@@ -3,11 +3,11 @@ SKIP_CASTLE_GUARD = false
 class ChallengeController < ApplicationController
   def get
     challenge_token = params[:challenge_token]
-    user_id = params[:user_id]
-    authentication = castle.authentications.find(challenge_token)
 
-    if authentication.user_id == user_id
-      user = User.find(user_id)
+    # TODO: Replace with real challenge token
+    authentication = castle.authentications.find(challenge_token)
+    if authentication
+      user = User.find(authentication.user_id)
       sign_in_without_castle_guard(user)
 
       redirect_to account_path
