557f79300885410bfff52e21b10f8dce8f54b96d
Timo Engelhardt <Timo.Engelhardt@gmail.com>
2016-06-19 20:56:01 +0200

    skip safeguard when resetting password

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,6 +1,14 @@
 class PasswordsController < Clearance::PasswordsController
   def update
-    super
-    @user.unlock!
+    @user = find_user_for_update
+
+    if @user.update_password password_reset_params
+      @user.unlock!
+      sign_in_without_castle_guard @user
+      redirect_to url_after_update
+    else
+      flash_failure_after_update
+      render template: 'passwords/edit'
+    end
   end
 end
